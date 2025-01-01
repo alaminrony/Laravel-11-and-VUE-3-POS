122591566c74ca59d3c7affe35ebfd83218ddd21
alaminrony <alaminrony100@gmail.com>
2025-01-01 19:54:21 +0600

    interface, repositiory, service class generated

---
+++ app/Repositories/Eloquent/SupplierLedgerRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+class SupplierLedgerRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ app/Services/SupplierLedgerService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class SupplierLedgerService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
