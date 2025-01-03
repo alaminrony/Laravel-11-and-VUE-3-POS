24b566fb9b2e4bda7966c7983e02c9ea39b1133d
alaminrony <alaminrony100@gmail.com>
2025-01-04 00:58:17 +0600

    readme file update

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+      <h1>Hello Vue 3!</h1>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    name: "App",
+  };
+  </script>
+
+  <style>
+  /* Add your styles here */
+  </style>
